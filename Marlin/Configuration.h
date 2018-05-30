831f3f4ff2499af9947666f4142bd44ba3ad4139
Mikhail Diatchenko <mikhail.diatchenko@gmail.com>
2018-05-30 14:38:35 +1200

    Calibrated PID for nozzle

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -355,9 +355,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define  DEFAULT_Kp 22.2
-  #define  DEFAULT_Ki 1.08
-  #define  DEFAULT_Kd 114
+  //#define  DEFAULT_Kp 22.2
+  //#define  DEFAULT_Ki 1.08
+  //#define  DEFAULT_Kd 114
 
   // MakerGear
   //#define  DEFAULT_Kp 7.0
@@ -368,6 +368,12 @@
   //#define  DEFAULT_Kp 63.0
   //#define  DEFAULT_Ki 2.25
   //#define  DEFAULT_Kd 440
+
+  // HyperCube
+
+  #define  DEFAULT_Kp 28.44
+  #define  DEFAULT_Ki 2.41
+  #define  DEFAULT_Kd 83.87
 
 #endif // PIDTEMP
 
